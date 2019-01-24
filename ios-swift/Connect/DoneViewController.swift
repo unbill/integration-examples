94a943d87f34b56fbf21280ef8bb34361f9b33ec
Scott Weinert <me@scottweinert.com>
2019-01-24 15:08:05 -0800

    fix swift

---
+++ ios-swift/Connect/DoneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DoneViewController.swift
+//  Connect
+//
+//  Created by Scott Weinert on 7/21/17.
+//  Copyright © 2017 Q2 Software Inc. All rights reserved.
+//
+
+import UIKit
+
+class DoneViewController: UIViewController {
+    var event: String?
+    
+    @IBOutlet weak var output: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Received event \(self.event)")
+        
+        self.output.text = self.event
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
